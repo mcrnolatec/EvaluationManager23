f1286dec52e371ece26103cc04223e6bf734b899
Marko Crnolatec <mcrnolate21@foi.hr>
2023-05-08 10:52:21 +0200

    Check user credentials If user is logged in(hardcoded), opens a new empty form.

--- EvaluationManager/EvaluationManager/FrmLogin.cs
+++ EvaluationManager/EvaluationManager/FrmLogin.cs
@@ -16,5 +16,26 @@ namespace EvaluationManager
         {
             InitializeComponent();
         }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if(username == "" || password == "")
+            {
+                MessageBox.Show("Popunite sva polja", "Pogreška", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            } else if(username == "nastavnik" &&  password == "test")
+            {
+                Hide();
+                FrmStudents frmstudents = new FrmStudents();
+                frmstudents.ShowDialog();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Korisničko ime ili lozinka nisu ispravni!", "Neuspjela prijava", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
