ba650ec60b020318f14b8d088029130e1aebf2bd
Marko Crnolatec <mcrnolate21@foi.hr>
2023-05-08 11:32:45 +0200

    Fix errors Fixed override ToString to work properly.

--- EvaluationManager/EvaluationManager/Models/Person.cs
+++ EvaluationManager/EvaluationManager/Models/Person.cs
@@ -11,9 +11,9 @@ namespace EvaluationManager.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-    }
-    public override string ToString()
-    {
-        return FirstName + " " + LastName;
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
     }
 }
